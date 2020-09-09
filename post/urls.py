ca27cebe38f09887ac5669cbe2d7ea2a39ad1fc1
arajo-hub <joara9566@naver.com>
2020-09-09 15:25:09 +0900

    Added css

--- post/urls.py
+++ post/urls.py
@@ -7,4 +7,5 @@ app_name='post'
 urlpatterns = [
     path('new/', views.save, name='savePost'),
     path('', views.PostList.as_view(), name='all'),
+    path('edit/', views.save, name='editPost'),
 ]
--- post/views.py
+++ post/views.py
@@ -20,6 +20,27 @@ def save(request):
             post=None
         return render(request, 'booklog/index.html', context)
 
+def edit(request, pk):
+    post=Post.objects.get(id=pk)
+    print(pk)
+    if request.method == "POST":
+        post=Post.objects.all()
+        context={'Post' : post}
+        try :
+            if post.review != request.POST['review']:
+                post=Post(user=request.user,
+                title=request.POST['title'],
+                author=request.POST['author'],
+                publisher=request.POST['publisher'],
+                pubdate=request.POST['pubdate'],
+                review=request.POST['review'])
+                post.save()
+            else:
+                messages.error("You didn't edit this post")
+        except:
+            post=None
+        return render(request, 'booklog/index.html', context)
+
 class PostList(SelectRelatedMixin, generic.ListView):
     model = models.Post
     select_related = ('user',)
